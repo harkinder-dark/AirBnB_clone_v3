3284882a23ca9b697ea64f2e542deb88256e2e32
Emmanuel (Demilade) Adebayo <92952014+AdebayoEmmanuel@users.noreply.github.com>
2023-02-15 09:02:07 +0100

    CREATE /stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,26 @@ that returns a JSON: "status": "OK" (see example)
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status',  methods=['GET'], strict_slashes=False)
 def status():
     """Returns JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['Get'], strict_slashes=False)
+def get_stats():
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    stats = {}
+    
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+    return jsonify(stats)
