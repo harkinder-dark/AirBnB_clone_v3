ed1d694e04fe8b06af1e44270560a8852b9a6113
harkinder-dark <victorvaddely@gmail.com>
2023-02-17 13:40:11 +0000

    after deadline

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,24 +12,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
-        ('Access-Control-Allow-Credentials', "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
-            "route": '/v1/views',
-        }
-    ]
-}
 swagger = Swagger(app)
 
 
@@ -48,8 +30,6 @@ def not_found(error):
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
-
     host = "0.0.0.0" if not HBNB_API_HOST else HBNB_API_HOST
     port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
-
     app.run(host=host, port=port, threaded=True)
