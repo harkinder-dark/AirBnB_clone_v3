c803cb64734b630be03a6821a6b934f14a03dc93
harkinder-dark <victorvaddely@gmail.com>
2023-02-23 21:53:32 +0000

    after deadline

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,8 @@ from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
-#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+#app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
