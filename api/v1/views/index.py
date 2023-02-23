f0debaa7b9f4c6d2fea424e330fe9641caae0350
harkinder-dark <victorvaddely@gmail.com>
2023-02-23 18:31:12 +0000

    after second deadline

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """create a file index.py"""
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -15,10 +22,10 @@ def status():
 def stats():
     """status getter"""
     return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     })
